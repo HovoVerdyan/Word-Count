250e6917a1b459609a58e73b5491b263922dc18d
Hovhannes <hovhannesverdyan@gmail.com>
2022-10-04 13:56:22 +0400

    Getting ready to commit new updates before refactoring

---
+++ HtmlFunctions.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace WordCount\Functions;
+
+class HtmlFunctions
+{
+
+}
--- Word-count.php
+++ Word-count.php
@@ -7,70 +7,90 @@ Version: 1.0
 Author: Hovhannes Verdyan
  */
 
-class WordCountPlugin {
 
-    public function __construct(){
-        add_action('admin_menu', [$this, 'adminPage']);
-        add_action('admin_init', [$this, 'settings']);
+class WordCountAndTimePlugin {
+    function __construct() {
+        add_action('admin_menu', array($this, 'adminPage'));
+        add_action('admin_init', array($this, 'settings'));
     }
 
-    public function settings()
-    {
+    function adminPage() {
+        add_options_page(
+            'Word Count Settings',
+            'Word Count',
+            'manage_options',
+            'word-count-settings-page',
+            array($this, 'ourHTML')
+        );
+    }
 
-        //Where to show the texts
+    function settings() {
         add_settings_section(
-            'wcp_first_section',
-            null,
-            null,
-            'word-count-setting-page'
+                'wcp_first_section',
+                null,
+                null,
+                'word-count-settings-page'
         );
 
+        //Choosing the location of the texts in the post
         add_settings_field(
-            'wcp_location',
-            'Display Location',
-            [$this, 'locationHTML'],
-            'word-count-setting-page',
-            'wcp_first_section'
+                'wcp_location',
+                'Display Location',
+                array($this, 'locationHTML'),
+                'word-count-settings-page',
+                'wcp_first_section'
         );
 
         register_setting(
-            'wordCountPlugin',
-            'wcp_location',
-            ['sanitize_callback' => 'sanitize_text_field', 'default' => '0']
+                'wordcountplugin',
+                'wcp_location',
+                array('sanitize_callback' => 'sanitize_text_field',
+                'default' => '0')
+        );
+
+        //Chosing the headline
+        add_settings_field(
+                'wcp_location',
+                'Headline Location',
+                array($this, 'headlineHTML'),
+                'word-count-settings-page',
+                'wcp_first_section'
+        );
+
+        register_setting(
+                'wordcountplugin',
+                'wcp_headline',
+                array('sanitize_callback' => 'sanitize_text_field',
+                'default' => 'Post Statistics')
         );
 
     }
 
-    public function locationHTML()
-    { ?>
+    public function headlineHTML()
+    {
+
+    }
+
+    public function locationHTML() { ?>
         <select name="wcp_location">
             <option value="0" <?php selected(get_option('wcp_location'), '0') ?>>Beginning of post</option>
-            <option value="1" <?php selected(get_option('wcp_location'), '1') ?>>The end of post</option>
+            <option value="1" <?php selected(get_option('wcp_location'), '1') ?>>End of post</option>
         </select>
     <?php }
 
-    public function adminPage()
-    {
-        add_options_page( 'Words count settings', 'Word Count', 'manage_options', 'word-count-setting-page', [$this, 'settings_html']);
-    }
 
-    public function settings_html()
-    { ?>
+    public function ourHTML() { ?>
         <div class="wrap">
             <h1>Word Count Settings</h1>
-            <form action="options.php">
+            <form action="options.php" method="POST">
                 <?php
-                settings_fields('wordCountPlugin');
-                do_settings_sections('word-count-setting-page');
+                settings_fields('wordcountplugin');
+                do_settings_sections('word-count-settings-page');
                 submit_button();
                 ?>
             </form>
         </div>
     <?php }
-
 }
 
-$wordCountAndTimePlugin = new WordCountPlugin();
-
-
-
+$wordCountAndTimePlugin = new WordCountAndTimePlugin();
