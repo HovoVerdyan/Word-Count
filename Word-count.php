7249eb146bd48a0f3de183e8b8fc40a251a81904
Hovhannes <hovhannesverdyan@gmail.com>
2022-10-05 18:05:14 +0400

    Finally counting all chharacters, words and readtime. Small security updates done.

--- Word-count.php
+++ Word-count.php
@@ -14,6 +14,55 @@ class WordCountAndTimePlugin {
     function __construct() {
         add_action('admin_menu', array($this, 'adminPage'));
         add_action('admin_init', array($this, 'settings'));
+        add_filter('the_content', array($this, 'ifWrap'));
+    }
+
+    function ifWrap($content)
+    {
+        if( is_main_query() AND is_single() AND
+        (
+            get_option('wcp_charactercount', '1') OR
+            get_option('wcp_readcount', '1') OR
+            get_option('wcp_wordcount', '1')
+        ))
+        {
+            return $this->createHTML($content);
+        }
+        return $content;
+    }
+
+    function createHTML($content)
+    {
+        $html = "<h3>" . esc_html(get_option('wcp_headline', 'Post Statistics')) . "</h3><p>";
+
+        //get word count because both wprdcount and readtime will read it
+        if( get_option('wcp_wordcount', '1') OR get_option('wcp_readtime', '1') )
+        {
+            $wordCount = str_word_count(strip_tags($content));
+        }
+
+        if ( get_option('wcp_wordcount', '1') )
+        {
+            $html .= 'This post has ' . $wordCount . ' words.<br>';
+        }
+
+        if ( get_option('wcp_charactercount', '1') )
+        {
+            $html .= 'This post has ' . strlen($content) . ' characters.<br>';
+        }
+
+        if ( get_option('wcp_readcount', '1') )
+        {
+            $html .= 'This post will take you ' . round($wordCount/255) . ' minutes to read.<br>';
+        }
+
+        $ttml .= '</p>';
+
+        if ( get_option('wcp_location', '0') == '0' )
+        {
+            return $html . $content;
+        }
+        return $content . $html;
     }
 
     function adminPage() {
